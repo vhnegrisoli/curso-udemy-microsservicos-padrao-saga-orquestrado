9ffe899dca5b64bf8d51a1820b684937670a35f6
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2023-04-15 17:33:44 -0300

    Adiciona arquivos base para o curso

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/config/kafka/KafkaConfig.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/config/kafka/KafkaConfig.java
@@ -1,4 +1,4 @@
-package br.com.microservices.orchestrated.inventoryservice.kafka;
+package br.com.microservices.orchestrated.inventoryservice.config.kafka;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/ProductValidationServiceApplication.java
@@ -2,9 +2,7 @@ package br.com.microservices.orchestrated.productvalidationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class ProductValidationServiceApplication {
 
